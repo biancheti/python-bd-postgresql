64a528f98f8385b1657187ea3e52467a0c635452
BiancheTI <alceubianchetfilho@gmail.com>
2024-06-27 21:53:39 -0300

    Login

--- menu.py
+++ menu.py
@@ -1,9 +1,25 @@
 from cadastro_categoria import menu_categoria
 from cadastro_produto import menu_produto
 from cadastro_cliente import menu_cliente
+from conexao import conecta_db
+
+def login(conexao) -> None:
+    login = input('Digite o login: ')
+    senha = input('Digite a senha: ')
+
+    cursor = conexao.cursor()
+    cursor.execute('select id, login, senha from usuario '  + 
+                   'where login = %s and senha = %s', (login, senha))
+    registro = cursor.fetchone()
+
+    if registro is None:
+        print('Usuario e senha invalida.')
+    else:
+        menu_principal()
 
 
-if __name__ == "__main__":
+
+def menu_principal():
     print('------------------------------')
     print('        MENU PROGRAMA         ')
     print('------------------------------')
@@ -29,3 +45,8 @@ if __name__ == "__main__":
             break
         else:
             print('Opção inválida, tente novamente.')
+
+
+if __name__ == "__main__":
+    conexao = conecta_db()
+    login(conexao)
