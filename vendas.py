f39b0c75d0cd88db0c28876293d852bf4d276301
BiancheTI <alceubianchetfilho@gmail.com>
2024-07-04 21:37:47 -0300

    Cadastro venda

--- vendas.py
+++ vendas.py
@@ -1,10 +1,48 @@
 from conexao import conecta_db
+from datetime import datetime
+
+def insert_venda(conexao, venda):
+    cursor = conexao.cursor()
+    sql_insert = """insert into venda (cliente_id, numero_venda, data_venda, valor_venda) values (%s, %s, %s, %s) RETURNING id; """
+    cursor.execute(sql_insert, venda)
+    venda_id = cursor.fetchone()[0]
+    conexao.commit()
+    return venda_id
 
 def consultar(conexao):
     print('Consultar')
 
-def inserir(conexao):
-    print('Inserir')
+def criar_venda(conexao):
+    venda = alimentar_venda()
+    itens_venda = alimentar_itens()
+    print(venda)
+    print(itens_venda)
+    venda_id = insert_venda(conexao, venda)
+    print(venda_id)
+
+def alimentar_venda():
+    id_cliente = input('Digite o ID do cliente: ')
+    numero_venda = 0
+    data_venda = datetime.now()
+    valor_venda = 0
+    return (id_cliente, numero_venda, data_venda, valor_venda)
+
+def alimentar_itens():
+    itens_venda = []
+    while(True):
+        produto_id = int(input('Digite o ID do produto: '))
+        quantidade = int(input('Digite a quantidade: '))
+        preco_unitario = float(input('Digite o preço unitário: '))
+        itens_venda.append({'produto_id': produto_id,
+                            'quantidade': quantidade,
+                            'preco_unitario': preco_unitario})
+        continua = input('Deseja adicionar mais intens? (S/N):')
+
+        if continua == 'N':
+            break
+
+    return itens_venda
+
 
 def menu_vendas(opcao):
     print("|--------------------------------|")
@@ -23,7 +61,7 @@ def menu_vendas(opcao):
         if opcao == "1":
             consultar(conexao)
         elif opcao == "2":
-            inserir(conexao)
+            criar_venda(conexao)
         elif opcao == "5":
             break
         else:
